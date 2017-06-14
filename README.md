b3f0b64b488656bdff4cf0696d9c1e6657c954ab
mjt <miksuu_79@hotmail.com>
2017-06-14 09:02:12 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# gta4cammod
-modifies gta4 camera
+GTA4cammod
+----------
+(c) 2010 mjt
+
+
+Modifies vehOff.csv file.
+
+usage:
+copy gta4cammod.exe and params.txt to $gta4_installation_dir$\common\data\
+edit params.txt there if needed.
+backup vehOff.csv file.
+run gta4cammod.exe
+run the game and test.
+
+pressing V you can change camera, and inside-cam is now outside the car, following it.
+
---
+++ gta4cammod_src/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * modifies gta4's inside car -camera (locked, bonnet cam) to where you want it to be (parameters from params.txt)
+ * mjt, 2010
+ * 
+ * params.txt format is
+ *     cam_xpos cam_zpos cam_height  x_rotation y_rotation z_rotation FOV
+ *  ie, 
+ *     0 -5 1.5 -0.1 0 0 95 
+ *  so camera is behind and above the car, looking a bit down.
+ *  
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace GTA4cammod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr;
+            string str, txt = "";
+            sr = File.OpenText("params.txt");
+            str = sr.ReadLine();
+            string[] par = str.Split(' ');
+            sr.Close();
+
+            sr = File.OpenText("vehoff.csv");
+            while (true)
+            {
+                str = sr.ReadLine();
+                if (str == null) break;
+
+                if (str.Contains("BONNET_FWD"))
+                {
+                    string[] bl = str.Split(' ');
+                    string tmp = "VEHICLE " + bl[1] + " FIELD BONNET_FWD POS " + par[0] + " " + par[1] + " " + par[2] +
+                        " ANGLES " + par[3] + " " + par[4] + " " + par[5] + " " +
+                        " FOV " + par[6];
+                    str = tmp;
+                }
+                txt += str + "\n";
+            }
+            sr.Close();
+
+            StreamWriter sw;
+            sw = File.CreateText("vehoff.csv");
+            sw.WriteLine(txt);
+            sw.Close();
+            Console.WriteLine("OK");
+        }
+    }
+}
